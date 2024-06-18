25ea78a872c06565be1a9cd8e48c8af4e5bffd0c
drong24 <daisyrong24@gmail.com>
2024-06-18 16:38:13 -0700

    added swift files, added MVC structure

---
+++ MyAdventure/MyAdventure/Model/Choice.swift
@@ -0,0 +1,8 @@
+//
+//  Choice.swift
+//  MyAdventure
+//
+//  Created by Daisy Rong on 6/18/24.
+//
+
+import Foundation
---
+++ MyAdventure/MyAdventure/Model/StoryLogic.swift
@@ -0,0 +1,8 @@
+//
+//  StoryLogic.swift
+//  MyAdventure
+//
+//  Created by Daisy Rong on 6/18/24.
+//
+
+import Foundation
