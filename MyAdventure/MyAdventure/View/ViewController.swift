63a476faf290eefa0b229af4f507603335a11f9e
drong24 <daisyrong24@gmail.com>
2024-06-18 19:17:38 -0700

    added text and button text of root node to view on load

--- MyAdventure/MyAdventure/View/ViewController.swift
+++ MyAdventure/MyAdventure/View/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var MainText: UILabel!
+    @IBOutlet weak var ButtonOneText: UIButton!
+    @IBOutlet weak var ButtonTwoText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         addNodes()
+        MainText.text = choices[0].value.text
+        ButtonOneText.setTitle(choices[0].value.option_one, for: .normal)
+        ButtonTwoText.setTitle(choices[0].value.option_two, for: .normal)
+        
         var count = 0 ... choices.count - 1
         for i in count {
             print(choices[i].value.text)
